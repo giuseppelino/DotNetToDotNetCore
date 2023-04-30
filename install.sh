d01d4bb0eba115e9350cba7a0f70eff46f09b549
GL <giuseppelino@gmail.com>
2023-04-29 22:48:54 -0400

    ok

--- install.sh
+++ install.sh
@@ -78,7 +78,7 @@ done
 
 #
 # Create an RSA key pair and save as secrets
-# 
+#
 echo 'Creating an RSA key pair for JWT token signing...'
 mkdir -p ./api/keys/private
 mkdir -p ./api/keys/public
@@ -110,8 +110,8 @@ echo 'Creating Firestore database...'
 n=0
 until [ "$n" -ge 5 ]
 do
-   gcloud alpha firestore databases create --region=$APPENGINE_REGION && break  
-   n=$((n+1)) 
+   gcloud alpha firestore databases create --location=$APPENGINE_REGION && break
+   n=$((n+1))
    sleep 30
 done
 
